d96203ff1d20b0b09e7b07da2e7383156ed90af3
Akshay Kumar <akshaykrkat@gmail.com>
2022-04-06 16:42:48 +0530

    UI changes in Product Component
    UI changes in Product Component

--- front-end/src/Components/Routes/Product/Product.js
+++ front-end/src/Components/Routes/Product/Product.js
@@ -1,8 +1,53 @@
+import appleImg from "../../../Resources/Images/apple.jpg";
+import "./Product.css";
+import Button from "../../UI/Button";
 const Product = () => {
   return (
-    <div>
-      <h1>Product Page</h1>
-    </div>
+    <main>
+      <aside>
+        <ul>
+          <li>
+            <a href="#">Fruits & Vegetables</a>
+          </li>
+          <li>
+            <a href="#">Bakery Cakes and Diary</a>
+          </li>
+          <li>
+            <a href="#">Beverages</a>
+          </li>
+          <li>
+            <a href="#">Beauty and Hygiene </a>
+          </li>
+          <li>
+            <a href="#">Baby Care</a>
+          </li>
+        </ul>
+      </aside>
+      <article className="product-container">
+        <section className="product-card">
+          <h1>Apple Washinton,Regular 4 Pcs</h1>
+          <div className="produt-description">
+            <img src={appleImg} alt="Common Hoope !" />
+            <span>
+              The bright red coloured and heart shaped Washinton apple are
+              crunchy and juicy
+            </span>
+          </div>
+          <Button title="Buy Now @ Rs 87 " />
+        </section>
+        <section className="product-card">
+          <h1>Apple Washinton,Regular 4 Pcs</h1>
+          <div className="produt-description">
+            <img src={appleImg} alt="Common Hoope !" />
+            <span>
+              The bright red coloured and heart shaped Washinton apple are
+              crunchy and juicy
+            </span>
+          </div>
+          <Button title="Buy Now @ Rs 87 " />
+        </section>
+      </article>
+    </main>
   );
 };
 
