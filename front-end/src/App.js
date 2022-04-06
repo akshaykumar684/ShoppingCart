67fdf0442f49dcdfa5c33fd4f98f7b7eb7a5c591
Akshay Kumar <akshaykrkat@gmail.com>
2022-04-06 14:34:34 +0530

    Created Initial UI for home page
    Created Initial UI for home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,21 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import Product from "./Product";
+import Home from "./Components/Routes/Home/Home";
+import Product from "./Components/Routes/Product/Product";
 import NavBar from "./Components/NavBar/NavBar";
 import React from "react";
 function App() {
   return (
     <React.Fragment>
       <NavBar />
-      <main>
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/product" exact>
-            <Product />
-          </Route>
-        </Switch>
-      </main>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/product" exact>
+          <Product />
+        </Route>
+      </Switch>
     </React.Fragment>
   );
 }
---
+++ front-end/src/Components/Routes/Home/Home.js
@@ -0,0 +1,80 @@
+import Offer5 from "../../../Resources/Images/offer5.jpg";
+import Beverages from "../../../Resources/Images/beverages.png";
+import "./Home.css";
+const Home = () => {
+  return (
+    <main>
+      <article>
+        <img src={Offer5} alt="Common Hoope !" className="offer-img" />
+        <section className="category">
+          <img src={Beverages} alt="Offer" />
+          <div className="product-description">
+            <h3>Beverages</h3>
+            <p>
+              Our beverage department will ensure your fridge is fully
+              stocked.Shop for Soda,juice,beer and many more.
+            </p>
+            <button className="explore-btn">Explore Beverages</button>
+          </div>
+        </section>
+        <section className="category">
+          <img src={Beverages} alt="Common Hoope !" />
+          <div className="product-description">
+            <h3>Beverages</h3>
+            <p>
+              Our beverage department will ensure your fridge is fully
+              stocked.Shop for Soda,juice,beer and many more.
+            </p>
+            <button className="explore-btn">Explore Beverages</button>
+          </div>
+        </section>
+        <section className="category">
+          <img src={Beverages} alt="Common Hoope !" />
+          <div className="product-description">
+            <h3>Beverages</h3>
+            <p>
+              Our beverage department will ensure your fridge is fully
+              stocked.Shop for Soda,juice,beer and many more.
+            </p>
+            <button className="explore-btn">Explore Beverages</button>
+          </div>
+        </section>
+        <section className="category">
+          <img src={Beverages} alt="Common Hoope !" />
+          <div className="product-description">
+            <h3>Beverages</h3>
+            <p>
+              Our beverage department will ensure your fridge is fully
+              stocked.Shop for Soda,juice,beer and many more.
+            </p>
+            <button className="explore-btn">Explore Beverages</button>
+          </div>
+        </section>
+        <section className="category">
+          <img src={Beverages} alt="Common Hoope !" />
+          <div className="product-description">
+            <h3>Beverages</h3>
+            <p>
+              Our beverage department will ensure your fridge is fully
+              stocked.Shop for Soda,juice,beer and many more.
+            </p>
+            <button className="explore-btn">Explore Beverages</button>
+          </div>
+        </section>
+        <section className="category">
+          <img src={Beverages} alt="Common Hoope !" />
+          <div className="product-description">
+            <h3>Beverages</h3>
+            <p>
+              Our beverage department will ensure your fridge is fully
+              stocked.Shop for Soda,juice,beer and many more.
+            </p>
+            <button className="explore-btn">Explore Beverages</button>
+          </div>
+        </section>
+      </article>
+    </main>
+  );
+};
+
+export default Home;
