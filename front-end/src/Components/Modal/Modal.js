dcb0d6c0c17f7d3765385133f64b22dbe28ea765
Akshay Kumar <akshaykrkat@gmail.com>
2022-04-07 23:10:57 +0530

    added axios lib for api fetching
    added axios lib for api fetching

--- front-end/src/Components/Modal/Modal.js
+++ front-end/src/Components/Modal/Modal.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Cart from "../Cart/Cart";
 import { useEffect } from "react";
+import axios from "../../axios/axios-congig";
 
 const Modal = () => {
   useEffect(() => {
     document.body.setAttribute("style", "overflow:hidden");
+    axios
+      .get(`/banners`)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
     return () => document.body.setAttribute("style", "overflow:auto");
   }, []);
   return (
